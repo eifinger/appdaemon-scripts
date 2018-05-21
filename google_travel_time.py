a91e492176163ba1a2f83faa31618d34ccabc5ae
Kevin Eifinger <k.eifinger@googlemail.com>
2018-05-21 14:19:12 +0200

    Added google_travel_time

---
+++ google_travel_time.py
@@ -0,0 +1,49 @@
+import appdaemon.plugins.hass.hassapi as hass
+import google_wrapper as gmaps
+
+#
+# App to turn something on when motion detected then off again after a delay if no more motion was detected
+#
+#
+# Args:
+#
+# entities: Entity state to update
+# entity.from : Location from where to drive
+# entity.to : Location to drive to
+# example:
+# entities:
+#  input_number.travel_from_home_to_work:
+#    from: Mainz
+#    to: Wiesbaden
+#
+# Release Notes
+#
+# Version 1.1:
+#   Add ability for other apps to cancel the timer
+#
+# Version 1.0:
+#   Initial Version
+
+class GoogleTravelTime(hass.Hass):
+
+    def initialize(self):
+    
+        self.handle = None
+        self.max_api_calls = 2500
+        self.delay = int(round((((3600 * 24) / self.max_api_calls) * 1,1)))
+        self.log("Delay is: {}".format(self.delay))
+
+        self.calculate_travel_times()
+        self.run_in(self.calculate_travel_times, self.delay)            
+
+    
+    def calculate_travel_times(self, *kwargs):
+        if "entity" in self.args:
+            self.delay = int(round(self.delay * len(self.args["entities"])))
+            self.log("Found {} entities to update. Setting delay to {}", format(len(self.args["entities"]), self.delay))
+            for entity in self.args["entities"]:
+                travelTime = int(round(self.gmaps.get_distance_matrix(entity["from"], entity["to"])["duration_in_traffic"]["value"] / 60))
+                self.log("Updating {} to {} minutes".format(entity, travelTime))
+                self.set_state(entity, travelTime)
+        else:
+            self.log("No entity defined", level = "ERROR")
---
+++ google_wrapper.py
@@ -0,0 +1,43 @@
+import googlemaps
+from datetime import datetime
+import secrets
+
+class GoogleWrapper:
+
+    def __init__(self):
+        self.gmaps = googlemaps.Client(secrets.GOOGLE_MAPS_API_TOKEN)
+
+    def get_distance_matrix(self, origin, destination):
+        now = datetime.now()
+        matrix = self.gmaps.distance_matrix(origin,
+                                            destination,
+                                            mode="driving",
+                                            departure_time=now,
+                                            language="de",
+                                            traffic_model = "best_guess")
+        distance = matrix['rows'][0]['elements'][0]['distance']
+        duration = matrix['rows'][0]['elements'][0]['duration']
+        duration_in_traffic = matrix['rows'][0]['elements'][0]['duration_in_traffic']
+        return {"distance": distance, "duration": duration, "duration_in_traffic": duration_in_traffic}
+
+    def get_geocode_for_location(self, location_name):
+        result = self.gmaps.geocode(location_name)
+        locations = []
+        if len(result) > 0:
+            for place in result:
+                location = {}
+                location["address"] = place["formatted_address"]
+                location["geocode"] = place["geometry"]["location"]
+                locations.append(location)
+            return locations
+        else:
+            result = self.gmaps.places(location_name)
+            if len(result["results"]) > 0:
+                for place in result["results"]:
+                    location = {}
+                    location["address"] = place["formatted_address"]
+                    location["geocode"] = place["geometry"]["location"]
+                    locations.append(location)
+                return locations
+            else:
+                return None
