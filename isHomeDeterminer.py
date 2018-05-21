3dc2550918764ae1ba24ddbaa05e28ef11a93fbd
Kevin Eifinger <k.eifinger@googlemail.com>
2018-05-21 17:09:46 +0200

    Added everone to isHomeDeterminer

--- isHomeDeterminer.py
+++ isHomeDeterminer.py
@@ -13,18 +13,27 @@ import appdaemon.plugins.hass.hassapi as hass
 class IsHomeDeterminer(hass.Hass):
 
     def initialize(self):
-        oneplus_state = self.get_state("device_tracker.android342cb6b2879c4a9b")
-        if oneplus_state == "home":
-            self.set_state(self.args["isHome"], state = "on")
-        if oneplus_state == "not_home":
-            self.set_state(self.args["isHome"], state = "off")
+        oneplus3 = self.get_state("device_tracker.android342cb6b2879c4a9b")
+        oneplus3T = self.get_state("device_tracker.android841a92d172870395")
+        self.isHomeHandler(oneplus3T, oneplus3)
         
         self.listen_state(self.state_change, "device_tracker.android342cb6b2879c4a9b")
+        self.listen_state(self.state_change, "device_tracker.android841a92d172870395")
     
     def state_change(self, entity, attribute, old, new, kwargs):
         if new != "":
-            if new == "home":
-                self.set_state(self.args["isHome"], state = "on")
-            if new == "not_home":
-                self.set_state(self.args["isHome"], state = "off")
+            if entity == "device_tracker.android841a92d172870395":
+                oneplus3 = self.get_state("device_tracker.android342cb6b2879c4a9b")
+                oneplus3T = new
+            if entity == "device_tracker.android342cb6b2879c4a9b":
+                oneplus3T = self.get_state("device_tracker.android841a92d172870395")
+                oneplus3 = new
+            self.isHomeHandler(oneplus3T, oneplus3)
+
+    def isHomeHandler(self, oneplus3T, oneplus3)
+        if oneplus3 == "home" or oneplus3T == "home":
+            self.set_state(self.args["isHome"], state = "on")
+        if oneplus3 == "not_home" and oneplus3T == "not_home":
+            self.set_state(self.args["isHome"], state = "off")
+            self.call_service("notify/slack",message="Everyone left home. Setting isHome to off")
       
