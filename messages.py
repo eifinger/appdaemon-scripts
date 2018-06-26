d2445b0bf3dea9337732c18f00b1257e72625f6e
Kevin Eifinger <k.eifinger@googlemail.com>
2018-06-26 13:12:38 +0200

    Remove @here from messages

--- messages.py
+++ messages.py
@@ -2,10 +2,10 @@ language = "de"
 
 def journey_start():
     if language == "de":
-        return "@here Du kannst losfahren nach {}"
+        return "Du kannst losfahren nach {}"
     if language == "en":
-        return "@here You can start your journey to {}"
-    return "@here You can start your journey to {}"
+        return "You can start your journey to {}"
+    return "You can start your journey to {}"
 
 def isHome_off():
     if language == "de":
@@ -16,24 +16,24 @@ def isHome_off():
 
 def unknown_device_connected():
     if language == "de":
-        return "@here Unbekanntes Gerät entdeckt: {}"
+        return "Unbekanntes Gerät entdeckt: {}"
     if language == "en":
-        return "@here Unknown device connected: {}"
-    return "@here Unknown device connected: {}"
+        return "Unknown device connected: {}"
+    return "Unknown device connected: {}"
 
 def device_change_alert():
     if language == "de":
-        return "@here Alarm: {} ist gewechselt auf {}"
+        return "Alarm: {} ist gewechselt auf {}"
     if language == "en":
-        return "@here Alarm: {} changed to {}"
-    return "@here Alarm: {} changed to {}"
+        return "Alarm: {} changed to {}"
+    return "Alarm: {} changed to {}"
 
 def failed_login_detected():
     if language == "de":
-        return "@here Alarm: {}"
+        return "Alarm: {}"
     if language == "en":
-        return "@here Alarm: {}"
-    return "@here Alarm: {}"
+        return "Alarm: {}"
+    return "Alarm: {}"
 
 def welcome_home():
     if language == "de":
@@ -44,10 +44,10 @@ def welcome_home():
 
 def forgot_window_open():
     if language == "de":
-        return "@here Du hast {} offen gelassen Dummie."
+        return "Du hast {} offen gelassen Dummie."
     if language == "en":
-        return "@here You left open {} Dummy."
-    return "@here You left open {} Dummy."
+        return "You left open {} Dummy."
+    return "You left open {} Dummy."
 
 def forgot_light_on():
     if language == "de":
@@ -58,24 +58,24 @@ def forgot_light_on():
 
 def user_is_leaving_zone():
     if language == "de":
-        return "@here {} ist gerade von {} weg."
+        return "{} ist gerade von {} weg."
     if language == "en":
-        return "@here {} just left {}"
-    return "@here {} just left {}" 
+        return "{} just left {}"
+    return "{} just left {}" 
 
 def user_is_heading_to_zone():
     if language == "de":
-        return "@here {} ist auf dem Weg nach {}."
+        return "{} ist auf dem Weg nach {}."
     if language == "en":
-        return "@here {} is on his way to {}"
-    return "@here {} is on his way to {}" 
+        return "{} is on his way to {}"
+    return "{} is on his way to {}" 
 
 def user_is_still_heading_to_zone():
     if language == "de":
-        return "@here {} ist auf immer noch dem Weg nach {}."
+        return "{} ist auf immer noch dem Weg nach {}."
     if language == "en":
-        return "@here {} is still on his way to {}"
-    return "@here {} is still on his way to {}" 
+        return "{} is still on his way to {}"
+    return "{} is still on his way to {}" 
 
     
     
