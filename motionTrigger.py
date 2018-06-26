e4a20f53aa147740ec6933a4d8d2bfd987fd5410
Kevin Eifinger <k.eifinger@googlemail.com>
2018-06-26 12:55:12 +0200

    Actually implement the sundown check in motiontrigger dummy!

--- motionTrigger.py
+++ motionTrigger.py
@@ -50,7 +50,7 @@ class MotionTrigger(hass.Hass):
     def motion_detected(self, event_name, data, kwargs):
         self.log("Motion: event_name: {}, data: {}".format(event_name,data), level = "DEBUG")
         if data["entity_id"] == self.args["sensor"]:
-            if "after_sundown" in self.args and self.args["after_sundown"] == True:
+            if "after_sundown" in self.args and self.args["after_sundown"] == True and self.sun_down():
                 self.log("Motion detected: turning {} on".format(self.args["entity_on"]))
                 self.turn_on(self.args["entity_on"])
             else:
