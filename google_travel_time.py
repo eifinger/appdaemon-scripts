6c1ccc589f0d534cbe27faf3aff193a1f6f0896b
Kevin Eifinger <k.eifinger@googlemail.com>
2018-06-29 12:33:07 +0200

    Functionality for google_travel_time

--- google_travel_time.py
+++ google_travel_time.py
@@ -10,6 +10,7 @@ import messages
 # Args:
 # sensor: google travel sensor to watch. example: sensor.travel_time_home_from_work
 # notify_input_boolean: input_boolean determining whether to notify. example: input_boolean.travel_time_home_from_work
+# notify_name: Who to notify. example: group_notifications
 # acceptable_range (optional): Multiplier of the normal travel time that is still acceptable. example: 1.2
 #
 # Release Notes
@@ -32,6 +33,7 @@ class GoogleTravelTime(hass.Hass):
 
         self.sensor = self.get_arg("sensor")
         self.notify_input_boolean = self.get_arg("notify_input_boolean")
+        self.notify_name = self.get_arg("notify_name")
         try:
             self.acceptable_range = self.get_arg("acceptable_range")
         except KeyError:
@@ -44,29 +46,19 @@ class GoogleTravelTime(hass.Hass):
         self.log("old: {}".format(old))
         self.log("new: {}".format(new))
 
-    
-    def calculate_travel_times(self, *kwargs):
-        if "entities" in self.args:
-            for entity in self.args["entities"]:
-                _from = self.args["entities"][entity]["from"]
-                if _from.startswith("secret_"):
-                    _from = self.get_secret(_from)
-                _to = self.args["entities"][entity]["to"]
-                if _to.startswith("secret_"):
-                    _to = self.get_secret(_to)
-                travelTime = self.get_distance_matrix(_from, _to)
-                roundedTravelTime = int(round(travelTime["duration_in_traffic"]["value"] / 60))
-                self.log("Updating {} to {} minutes".format(entity, str(roundedTravelTime)))
-                self.set_state(entity, state = roundedTravelTime)
-                #Notify component
-                if roundedTravelTime <= travelTime["duration"]["value"] * 1.2 and self.get_state(self.args["entities"][entity]["notify_input_boolean"]) == "on":
-                    message = messages.journey_start().format(_to)
-                    self.log("Notify user")
-                    self.call_service("notify/group_notifications",message=message)
-                    self.turn_off(self.args["entities"][entity]["notify_input_boolean"])
-        else:
-            self.log("No entities defined", level = "ERROR")
-        self.run_in(self.calculate_travel_times, self.delay) 
+        duration_in_traffic = new["attributes"]["duration_in_traffic"]
+        duration_in_traffic_minutes = duration_in_traffic.substring(0,duration_in_traffic.find(" "))
+        self.log("duration_in_traffic_minutes: {}".format(duration_in_traffic_minutes))
+
+        duration = new["attributes"]["duration"]
+        duration_minutes = duration.substring(0,duration.find(" "))
+        self.log("duration_minutes: {}".format(duration_minutes))
+
+        if duration_in_traffic_minutes <= duration_minutes * self.acceptable_range and self.get_state(self.notify_input_boolean) == "on":
+            message = messages.journey_start().format(_to)
+            self.log("Notify user")
+            self.call_service("notify/" + self.notify_name, message=message)
+            self.turn_off(self.notify_input_boolean)
 
     def get_arg(self, key):
         key = self.args[key]
