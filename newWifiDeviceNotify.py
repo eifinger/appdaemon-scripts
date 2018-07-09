4a1d59eeb3be19aff31645adcdb1cb6501226394
Kevin Eifinger <k.eifinger@googlemail.com>
2018-07-09 18:40:27 +0200

    newdevicenotify

--- newWifiDeviceNotify.py
+++ newWifiDeviceNotify.py
@@ -6,14 +6,13 @@ class DeviceNotify(appapi.AppDaemon):
   def initialize(self):
     self.listen_event_handle_list = []
 
-    self.listen_event_handle_list.append(self.listen_event(self.newDevice, "device_tracker"))
+    self.listen_event_handle_list.append(self.listen_event(self.newDevice, "device_tracker_new_device"))
 
   def newDevice(self, event_name, data, kwargs):
     self.log("event_name: {}".format(event_name))
     self.log("data: {}".format(data))
-    if event_name == "device_tracker_new_device":
-        message = messages.unknown_device_connected().format(data)
-        self.call_service("notify/group_notifications",message=message)
+    message = messages.unknown_device_connected().format(data)
+    self.call_service("notify/group_notifications",message=message)
 
   def terminate(self):
     for listen_event_handle in self.listen_event_handle_list:
