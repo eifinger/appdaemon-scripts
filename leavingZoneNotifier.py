f9f5615e1fa809e69187f9dfc4922368cff29e4c
Kevin Eifinger <k.eifinger@googlemail.com>
2018-07-10 19:46:43 +0200

    new[last_changed]

--- leavingZoneNotifier.py
+++ leavingZoneNotifier.py
@@ -41,7 +41,7 @@ class LeavingZoneNotifier(hass.Hass):
         self.listen_state_handle_list.append(self.listen_state(self.zone_state_change, self.device, attribute = "all"))
 
     def zone_state_change(self, entity, attributes, old, new, kwargs):
-        last_changed = self.convert_utc(new["attributes"]["last_changed"])
+        last_changed = self.convert_utc(new["last_changed"])
         self.log("Zone of {} changed from {} to {}.".format(self.friendly_name(entity),old,new))
         if new == self.zone:
             self.log("Setting user_entered_zone to {}".format(last_changed))
