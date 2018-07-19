bc3bb7e187df46ee855b224bb24d368d3d86e9d5
Kevin Eifinger <k.eifinger@googlemail.com>
2018-07-19 17:49:16 +0200

    add faceboxMultipleSnapshots for trial purposes

--- travis_secrets.py
+++ travis_secrets.py
@@ -15,4 +15,5 @@ secret_dict = {
     "secret_facebox_ip": "192.168.0.1",
     "secret_facebox_port": "12345",
     "secret_facebox_notifier_filename": "/config/www/facebox/tmp/image.jpg",
-    "secret_telegram_user_id": -21415521}
+    "secret_telegram_user_id": -21415521,
+    "secret_facebox_folderpath_multiple": "/config/www/facebox/multiple"}
