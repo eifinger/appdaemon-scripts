3d69e8ef8f45fbbcf9ed9b9e3706e7545d0e02bf
Kevin Eifinger <k.eifinger@googlemail.com>
2018-07-19 10:28:34 +0200

    introduce travis_secrets.py

--- travis_secrets.py
+++ travis_secrets.py
@@ -1,9 +1,18 @@
-def get_arg(self, args, key):
-        key = args[key]
-        if key.startswith("secret_"):
-            if key in self.secret_dict:
-                return self.secret_dict[key]
-            else:
-                self.log("Could not find {} in secret_dict".format(key))
-        else:
-            return key
+GOOGLE_MAPS_API_TOKEN = "ASDfuksavf876584asfASLFzgsa876aASVzluas"
+
+secret_dict = {
+    "secret_work":"Frankfurt am Main, Germany",
+    "secret_home": "Mainz, Germany",
+    "secret_sindlingen": "Frankfurt am Main",
+    "secret_device_user_one": "device_tracker.me",
+    "secret_device_user_two": "device_tracker.notme",
+    "secret_name_user_one": "me",
+    "secret_name_user_two": "notme",
+    "secret_elmo_location": "00.000000, 00.000000",
+    "secret_facebox_folderpath": "/config/www/facebox",
+    "secret_facebox_unknown_directory": "/config/www/facebox/unknown",
+    "secret_facebox_noface_directory": "/config/www/facebox/noface",
+    "secret_facebox_ip": "192.168.0.1",
+    "secret_facebox_port": "12345",
+    "secret_facebox_notifier_filename": "/config/www/facebox/tmp/image.jpg",
+    "secret_telegram_user_id": -21415521}
