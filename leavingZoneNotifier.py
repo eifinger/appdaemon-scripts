18cd5b9f8b9b34abf492cf32a5e27470641d57b2
Kevin Eifinger <k.eifinger@googlemail.com>
2018-07-26 18:30:34 +0200

    remove unused import

--- leavingZoneNotifier.py
+++ leavingZoneNotifier.py
@@ -2,7 +2,6 @@ import appdaemon.plugins.hass.hassapi as hass
 import messages
 import datetime
 import globals
-import secrets
 #
 # App to notify if user_one is leaving a zone. User had to be in that zone 3 minutes before in order for the notification to be triggered
 #
