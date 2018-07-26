bc4f26d974dafa6f9930385f8ac9fba8778e7890
Kevin Eifinger <k.eifinger@googlemail.com>
2018-07-26 13:18:22 +0200

    using globals for notifyFailedLogin

--- notifyFailedLogin.py
+++ notifyFailedLogin.py
@@ -1,13 +1,16 @@
 import appdaemon.plugins.hass.hassapi as hass
 import messages
-import secrets
+import globals
 #
 # App to send notification when a login attemp fails
 #
 # Args:
-#  user_name: who to notifiy
+#  notify_name: who to notifiy
 #
 # Release Notes
+#
+# Version 1.1:
+#   Using globals now
 #
 # Version 1.0:
 #   Initial Version
@@ -15,25 +18,29 @@ import secrets
 class NotifyFailedLogin(hass.Hass):
 
   def initialize(self):
-    self.user_name = self.args["user_name"]
-    if self.user_name.startswith("secret_"):
-          self.user_name = self.get_secret(self.user_name)
+
+    self.timer_handle_list = []
+    self.listen_event_handle_list = []
     self.listen_state_handle_list = []
+
+
+    self.notify_name = globals.get_arg(self.args,"notify_name")
+    
     self.listen_state_handle_list.append(self.listen_state(self.state_change, "persistent_notification.httplogin"))
     
   def state_change(self, entity, attribute, old, new, kwargs):
     if new != "" and new == "notifying":
         message = self.get_state("persistent_notification.httplogin",attribute="message")
         self.log(message)
-        self.call_service("notify/" + self.user_name,message=messages.failed_login_detected().format(message))
-
-  def get_secret(self, key):
-      if key in secrets.secret_dict:
-          return secrets.secret_dict[key]
-      else:
-          self.log("Could not find {} in secret_dict".format(key))
+        self.call_service("notify/" + self.notify_name,message=messages.failed_login_detected().format(message))
 
   def terminate(self):
+    for timer_handle in self.timer_handle_list:
+      self.cancel_timer(timer_handle)
+
+    for listen_event_handle in self.listen_event_handle_list:
+      self.cancel_listen_event(listen_event_handle)
+
     for listen_state_handle in self.listen_state_handle_list:
       self.cancel_listen_state(listen_state_handle)
       
