d99dcff6b39ad5493d4d635199318932beb3eb41
Kevin Eifinger <k.eifinger@googlemail.com>
2018-07-26 09:26:38 +0200

    remove debug for logging

--- leavingZoneNotifier.py
+++ leavingZoneNotifier.py
@@ -42,7 +42,7 @@ class LeavingZoneNotifier(hass.Hass):
 
     def zone_state_change(self, entity, attributes, old, new, kwargs):
         last_changed = self.convert_utc(new["last_changed"])
-        self.log("Zone of {} changed from {} to {}.".format(self.friendly_name(entity),old,new), level = "DEBUG")
+        self.log("Zone of {} changed from {} to {}.".format(self.friendly_name(entity),old,new))
         if new == self.zone:
             self.log("Setting user_entered_zone to {}".format(last_changed))
             self.user_entered_zone = last_changed
