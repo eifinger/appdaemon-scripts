dab17d9c2c46bfe7fb93ba15987a53e3171464d1
Kevin Eifinger <k.eifinger@googlemail.com>
2018-07-27 16:30:48 +0200

    v1.3 of cominghome and added comingHomeAlexa

--- comingHome.py
+++ comingHome.py
@@ -8,7 +8,11 @@ import globals
 # sensor: door sensor
 # isHome: input_boolean which shows if someone is home eg input_boolean.isHome
 # actor: actor to turn on
+# after_sundown (optionally): whether to only trigger after sundown. example: True
 # Release Notes
+#
+# Version 1.2:
+#   Added after_sundown
 #
 # Version 1.1:
 #   Using globals
@@ -24,13 +28,21 @@ class ComingHome(hass.Hass):
     self.sensor = globals.get_arg(self.args,"sensor")
     self.isHome = globals.get_arg(self.args,"isHome")
     self.actor = globals.get_arg(self.args,"actor")
+    try:
+      self.after_sundown = globals.get_arg(self.args,"after_sundown")
+    except KeyError as identifier:
+      self.after_sundown = None
 
     self.listen_state_handle_list.append(self.listen_state(self.state_change, self.sensor))
     
   def state_change(self, entity, attribute, old, new, kwargs):
     if new != "":
       isHome_state = self.get_state(self.isHome)
-      if isHome_state == "off" and self.sun_down():
+      if self.after_sundown != None and self.after_sundown:
+        if self.sun_down():
+          self.log("{} changed to {}".format(self.friendly_name(entity), new))
+          self.turn_on(self.actor)
+      else:
         self.log("{} changed to {}".format(self.friendly_name(entity), new))
         self.turn_on(self.actor)
 
