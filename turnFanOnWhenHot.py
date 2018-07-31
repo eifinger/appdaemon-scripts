7b75b5896d616b7b109c5a56f2b4da47a670fd2a
Kevin Eifinger <k.eifinger@googlemail.com>
2018-07-31 15:49:26 +0200

    TurnFanOnWhenHot Added delay

--- turnFanOnWhenHot.py
+++ turnFanOnWhenHot.py
@@ -1,7 +1,9 @@
 import appdaemon.plugins.hass.hassapi as hass
 import globals
+import datetime
+
 #
-# App to Turn on fan when temp is above a threshold and someone is Home
+# App to Turn on fan when temp is above a threshold and someone is in the room
 #
 # Args:
 #
@@ -11,7 +13,11 @@ import globals
 # location_sensors: location sensors of users. example: sensor.location_user_one,sensor.location_user_two
 # room: Room name in which one of the users must be. example: Wohnzimmer
 # actor: actor to turn on
+# delay: seconds to wait before turning off. example: 120
 # Release Notes
+#
+# Version 1.3:
+#   Added delay
 #
 # Version 1.2:
 #   Using entities from HA now. Added turned_on_by_me
@@ -26,6 +32,7 @@ class TurnFanOnWhenHot(hass.Hass):
 
   def initialize(self):
     self.listen_state_handle_list = []
+    self.timer_handle_list = []
 
     self.app_switch = globals.get_arg(self.args,"app_switch")
     self.temp_sensor = globals.get_arg(self.args,"temp_sensor")
@@ -33,8 +40,11 @@ class TurnFanOnWhenHot(hass.Hass):
     self.location_sensors = globals.get_arg_list(self.args,"location_sensors")
     self.room = globals.get_arg(self.args,"room")
     self.actor = globals.get_arg(self.args,"actor")
+    self.delay = globals.get_arg(self.args,"delay")
 
     self.turned_on_by_me = False #Giggedi
+
+    self.turn_off_timer_handle = None
 
     self.listen_state_handle_list.append(self.listen_state(self.state_change, self.temp_sensor))
     for sensor in self.location_sensors:
@@ -55,14 +65,27 @@ class TurnFanOnWhenHot(hass.Hass):
               self.turn_on(self.actor)
               self.turned_on_by_me = True
             turn_on = True
+            if self.turn_off_timer_handle != None:
+              self.timer_handle_list.remove(self.turn_off_timer_handle)
+              self.cancel_timer(self.turn_off_timer_handle)
       if not turn_on and self.turned_on_by_me:
         if self.get_state(self.actor) != "off":
-          self.log("Turning off {}".format(self.friendly_name(self.actor)))
-          self.turn_off(self.actor)
-          self.turned_on_by_me = False
+          self.turn_off_timer_handle = self.run_in(self.turn_off_callback(),self.delay)
+          self.timer_handle_list.append(self.turn_off_timer_handle)
+            
+
+  def turn_off_callback(self, kwargs):
+    """Turn off the actor again if the timer was not cancelled in the meantime"""
+    self.log("Turning off {}".format(self.friendly_name(self.actor)))
+    self.turn_off(self.actor)
+    self.turned_on_by_me = False
+    self.turn_off_timer_handle = None
 
 
   def terminate(self):
     for listen_state_handle in self.listen_state_handle_list:
       self.cancel_listen_state(listen_state_handle)
+
+    for timer_handle in self.timer_handle_list:
+            self.cancel_timer(timer_handle)
       
