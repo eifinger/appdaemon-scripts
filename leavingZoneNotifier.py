101c2cabd24c5b1fec53d588e47d8693619c0cef
Kevin Eifinger <k.eifinger@googlemail.com>
2018-08-03 12:05:17 +0200

    note for delay and better handling of zone_entered for false positives

--- leavingZoneNotifier.py
+++ leavingZoneNotifier.py
@@ -9,12 +9,16 @@ import globals
 #   app_switch: on/off switch for this app. example: input_boolean.turn_fan_on_when_hot
 #   device: Device to track
 #   user_name: Name of the user used in the notification message
-#   delay: time to wait before notifying. Maybe user returns to zone. example: 120
+#   delay: seconds to wait before notifying. Maybe user returns to zone This should be too small to avoid false positives from your tracker. 
+#          I am using GPS Logger on Android and sometimes my device switches from work to home and 2 minutes later back. example: 120
 #   lingering_time: time a user has to be in a zone to trigger this app. example: 3600
 #   zone: zone name from which the user is leaving
 #   notify_name: Who to notify. example: group_notifications
 #
 # Release Notes
+#
+# Version 1.43:
+#   additional note for delay and better handling of zone_entered for false positives
 #
 # Version 1.3:
 #   delay and lingering_time now as args
@@ -32,6 +36,9 @@ class LeavingZoneNotifier(hass.Hass):
 
     def initialize(self):
 
+        self.listen_state_handle_list = []
+        self.timer_handle_list = []
+
         self.app_switch = globals.get_arg(self.args,"app_switch")
         self.user_name = globals.get_arg(self.args,"user_name")
         self.zone = globals.get_arg(self.args,"zone")
@@ -42,9 +49,9 @@ class LeavingZoneNotifier(hass.Hass):
         self.delay = globals.get_arg(self.args,"delay")
 
         self.user_entered_zone = None
+        self.false_positive = False
 
-        self.listen_state_handle_list = []
-        self.timer_handle_list = []
+        
 
         self.listen_state_handle_list.append(self.listen_state(self.zone_state_change, self.device, attribute = "all"))
 
@@ -52,13 +59,15 @@ class LeavingZoneNotifier(hass.Hass):
         if self.get_state(self.app_switch) == "on":
             last_changed = self.convert_utc(new["last_changed"])
             self.log("Zone of {} changed from {} to {}.".format(self.friendly_name(entity),old["state"],new["state"]), level="DEBUG")
-            if new["state"] == self.zone:
-                self.log("Setting user_entered_zone to {}".format(last_changed), level = "DEBUG")
+            if new["state"] == self.zone and old["state"] != self.zone and self.false_positive == False:
+                self.log("Setting user_entered_zone to {}".format(last_changed))
                 self.user_entered_zone = last_changed
-            if old["state"] == self.zone:
+            if old["state"] == self.zone and new["state"] != self.zone:
                 if self.user_entered_zone == None or (last_changed - self.user_entered_zone >= datetime.timedelta(seconds=self.lingering_time)):
                     self.log("Zone of {} changed from {} to {}. Wait {} seconds until notification.".format(self.friendly_name(entity),old,new,self.delay))
                     self.timer_handle_list.append(self.run_in(self.notify_user, self.delay, old_zone = old))
+                    self.false_positive = True
+                    self.log("Setting false_positive to {}".format(self.false_positive))
 
         
 
@@ -66,7 +75,10 @@ class LeavingZoneNotifier(hass.Hass):
         #Check if user did not come back to the zone in the meantime
         if self.get_state(self.device) != kwargs["old_zone"]:
             self.log("Notify user")
-            self.call_service("notify/" + self.notify_name, message=messages.user_is_leaving_zone().format(self.user_name, self.zone)) 
+            self.call_service("notify/" + self.notify_name, message=messages.user_is_leaving_zone().format(self.user_name, self.zone))
+            self.false_positive = False
+            self.log("Setting false_positive to {}".format(self.false_positive))
+            
 
 
     def terminate(self):
