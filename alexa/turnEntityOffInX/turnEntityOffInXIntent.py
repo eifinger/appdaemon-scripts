3698dc2a18f238f71d2593a3e12f7d9a1efe399f
Kevin Eifinger <k.eifinger@googlemail.com>
2018-08-03 14:42:22 +0200

    Add developtment lightStateIntent

---
+++ alexa/turnEntityOffInX/turnEntityOffInXIntent.py
@@ -0,0 +1,29 @@
+import appdaemon.plugins.hass.hassapi as hass
+import random
+
+class lightStateIntent(hass.Hass):
+
+    def initialize(self):
+        return
+
+    def getIntentResponse(self, slots, devicename):
+        ############################################
+        # an Intent to give back the state from a light.
+        # but it also can be any other kind of entity
+        ############################################           
+        try:
+            self.log("slots: {}".format(slots)
+            entityname = self.args["entities"][slots["device"]]
+            duration = [slots["duration"]]
+            text = self.random_arg(self.args["textLine"]) + state            
+        except:
+            text = self.random_arg(self.args["Error"])
+        return text
+
+    def floatToStr(self,myfloat):
+        ############################################
+        # replace . with , for better speech
+        ############################################
+        floatstr = str(myfloat)
+        floatstr = floatstr.replace(".",",")
+        return floatstr
