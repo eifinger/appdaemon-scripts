fedfcc9bd774654648755faaad406f6af2834657
Kevin Eifinger <k.eifinger@googlemail.com>
2018-08-16 12:42:26 +0200

    self.__NOTIFY__

--- notifier/notifier.py
+++ notifier/notifier.py
@@ -18,23 +18,23 @@ import globals
 
 class Notifier(hass.Hass):
 
-    __ALEXA_TTS__ = "media_player.alexa_tts"
-    __NOTIFY__ = "notify/"
-
     def initialize(self):
         self.media_player = globals.get_arg(self.args,"media_player")
         self.source = globals.get_arg(self.args,"source")
         self.alexa_media_player = globals.get_arg(self.args,"alexa_media_player")
 
+        self.__NOTIFY__ = "notify/"
+        self.__ALEXA_TTS__ = "media_player.alexa_tts"
+
     def notify(self, notify_name, message, useAlexa=True, useTelegram=True):
         if useTelegram:
             self.log("Notifying via Telegram")
-            self.call_service(__NOTIFY__ + notify_name,message=message)
+            self.call_service(self.__NOTIFY__ + notify_name,message=message)
         if useAlexa:
             self.log(self.get_state(self.media_player, attribute = "all"))
             if self.get_state(self.media_player) == "on":
                 if self.get_state(self.media_player, attribute = "all")["attributes"]["source"] == self.source:
-                    self.call_service(__ALEXA_TTS__, entity=self.alexa_media_player, message=message)
+                    self.call_service(self.__ALEXA_TTS__, entity=self.alexa_media_player, message=message)
 
 
     
