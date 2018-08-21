e21c4f4977bf649919b1371daf6515b154bb3dc5
Kevin Eifinger <k.eifinger@googlemail.com>
2018-08-21 09:20:49 +0200

    [comingHome] Version 1.3.1 Actually implement isHome

--- comingHome/comingHome.py
+++ comingHome/comingHome.py
@@ -1,5 +1,6 @@
 import appdaemon.plugins.hass.hassapi as hass
 import globals
+import datetime
 #
 # App to Turn on Lobby Lamp when Door openes and no one is Home
 #
@@ -11,6 +12,9 @@ import globals
 # actor: actor to turn on
 # after_sundown (optionally): whether to only trigger after sundown. example: True
 # Release Notes
+#
+# Version 1.3.1:
+#   Actually implement isHome
 #
 # Version 1.3:
 #   Added app_switch
@@ -38,19 +42,24 @@ class ComingHome(hass.Hass):
     except KeyError as identifier:
       self.after_sundown = None
 
+    self.delay = 2
+
     self.listen_state_handle_list.append(self.listen_state(self.state_change, self.sensor))
     
   def state_change(self, entity, attribute, old, new, kwargs):
     if self.get_state(self.app_switch) == "on":
       if new != "":
-        isHome_state = self.get_state(self.isHome)
-        if self.after_sundown != None and self.after_sundown:
-          if self.sun_down():
+        isHome_attributes = self.get_state(self.isHome, attribute = "all")
+        isHome_state = isHome_attributes["state"]
+        last_changed = isHome_attributes["last_changed"]
+        if isHome_state = "off" or (datetime.datetime.now() - last_changed >= datetime.timedelta(seconds=self.delay)):
+          if self.after_sundown != None and self.after_sundown:
+            if self.sun_down():
+              self.log("{} changed to {}".format(self.friendly_name(entity), new))
+              self.turn_on(self.actor)
+          else:
             self.log("{} changed to {}".format(self.friendly_name(entity), new))
             self.turn_on(self.actor)
-        else:
-          self.log("{} changed to {}".format(self.friendly_name(entity), new))
-          self.turn_on(self.actor)
 
   def terminate(self):
     for listen_state_handle in self.listen_state_handle_list:
