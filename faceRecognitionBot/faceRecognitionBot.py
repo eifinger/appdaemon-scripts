a1a9d2a59d3fb5ce93f1029636c5d6f7ee33290f
Kevin Eifinger <k.eifinger@googlemail.com>
2018-08-22 19:17:42 +0200

    still debugging

--- faceRecognitionBot/faceRecognitionBot.py
+++ faceRecognitionBot/faceRecognitionBot.py
@@ -183,13 +183,16 @@ class FaceRecognitionBot(hass.Hass):
 
     def _getNumberOfDistinctFaces(self, result_dict_dict):
         """Check how many distinct faces got identified"""
-        id_list = [d["faces"]["id"] for d in result_dict_dict.values()]
-        return len(list(set(id_list)))
+        return len(self._getFaceNamesFromResult(result_dict_dict))
 
     def _getFaceNamesFromResult(self, result_dict_dict):
         """Return a list of names for the identified faces"""
         try:
-            return list(set([d["faces"]["id"] for d in result_dict_dict.values()]))
+            id_list = []
+            for d in result_dict_dict.values():
+                for faces in d["faces"]:
+                    id_list.append(faces["id"])
+            return list(set(id_list))
         except TypeError:
             return []
 
