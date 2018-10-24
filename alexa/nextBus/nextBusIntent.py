260d852acc13348f28c3186e3e6019640901d04a
Kevin Eifinger <k.eifinger@googlemail.com>
2018-10-24 18:59:36 +0200

    added nextBusIntent

---
+++ alexa/nextBus/nextBusIntent.py
@@ -0,0 +1,23 @@
+import appdaemon.plugins.hass.hassapi as hass
+import random
+import globals
+
+class nextBusIntent(hass.Hass):
+
+    def initialize(self):
+        self.sensor = globals.get_arg(self.args,"sensor")
+        self.textLine = globals.get_arg(self.args,"textLine")
+        self.Error = globals.get_arg(self.args,"Error")
+
+    def getIntentResponse(self, slots, devicename):
+        ############################################
+        # give next bus departure
+        ############################################
+        try:
+            state = self.get_state(self.sensor, attribute="all")
+            line = state["line"]
+            minutes = state["minutes"]
+            text = self.textLine.format(line, minutes)
+        except:
+            text = self.Error
+        return text
