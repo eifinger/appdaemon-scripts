4c36d924ed8ca269b2cad3e0574d28e104355db1
Kevin Eifinger <k.eifinger@googlemail.com>
2018-11-03 16:09:27 +0100

    [IncreaseCounterOnServiceCall] add logging

--- increaseCounterOnServiceCall/increaseCounterOnServiceCall.py
+++ increaseCounterOnServiceCall/increaseCounterOnServiceCall.py
@@ -31,6 +31,7 @@ class IncreaseCounterOnServiceCall(hass.Hass):
     self.listen_event_handle_list.append(self.listen_event(self.call_service_callback, "call_service"))
     
   def call_service_callback(self, event_name, data, kwargs):
+    self.log(event_name + ': ' + str(data))
     if (self.get_state(self.app_switch) == "on" and data["domain"] == self.domain 
     and data["service"] == self.service and data["service_data"]["entity_id"] == self.entity_id):
       self.call_service("counter/increment", entity_id=self.counter)
