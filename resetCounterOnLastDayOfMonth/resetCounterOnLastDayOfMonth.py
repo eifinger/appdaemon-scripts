95c96a0ab0dd93364a4009e996e5f3b41172f97a
Kevin Eifinger <k.eifinger@googlemail.com>
2018-11-03 16:03:08 +0100

    typo

--- resetCounterOnLastDayOfMonth/resetCounterOnLastDayOfMonth.py
+++ resetCounterOnLastDayOfMonth/resetCounterOnLastDayOfMonth.py
@@ -36,7 +36,7 @@ class ResetCounterOnLastDayOfMonth(hass.Hass):
       self.log("Next reset will be: {}".format(runtime))
       self.timer_handle_list.append(self.run_at(self.run_at_callback, runtime))
 
-  def last_day_of_month(any_day):
+  def last_day_of_month(self, any_day):
     next_month = any_day.replace(day=28) + datetime.timedelta(days=4)  # this will never fail
     return next_month - datetime.timedelta(days=next_month.day)
 
