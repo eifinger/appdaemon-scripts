59b5b742919709bc83a411feb660a6793a3dd022
Kevin Eifinger <k.eifinger@googlemail.com>
2018-11-09 18:05:33 +0100

    [NextApppointmentLeaveNotifier] Version 1.4: Don't include Google Maps Link in Notification for Alexa

--- nextApppointmentLeaveNotifier/nextApppointmentLeaveNotifier.py
+++ nextApppointmentLeaveNotifier/nextApppointmentLeaveNotifier.py
@@ -17,6 +17,9 @@ import globals
 #
 # Release Notes
 #
+# Version 1.4:
+#   Don't include Google Maps Link in Notification for Alexa
+#
 # Version 1.3:
 #   Also notify when Notification time is in the past
 #
@@ -35,13 +38,13 @@ class NextApppointmentLeaveNotifier(hass.Hass):
 
         self.listen_state_handle_list = []
         
-
         self.sensor = globals.get_arg(self.args,"sensor")
         self.notify_input_boolean = globals.get_arg(self.args,"notify_input_boolean")
         self.notify_name = globals.get_arg(self.args,"notify_name")
         self.destination_name_sensor = globals.get_arg(self.args,"destination_name_sensor")
         self.travel_time_sensor = globals.get_arg(self.args,"travel_time_sensor")
-        self.message = globals.get_arg(self.args,"message_DE")
+        self.message = globals.get_arg(self.args,"message")
+        self.message_google_link = globals.get_arg(self.args,"message_google_link")
 
         self.timer_handle = None
 
@@ -82,10 +85,7 @@ class NextApppointmentLeaveNotifier(hass.Hass):
                     self.log("Will notify at {}".format(notification_time))
                 except ValueError:
                     self.log("Notification time is in the past")
-                    google_maps_url = self.google_source_url + location_name.replace(" ","+")
-                    self.log("Notify user")
-                    self.notifier.notify(self.notify_name, self.message.format(location_name,self.get_state(self.travel_time_sensor), google_maps_url))
-                    self.location_of_last_notified_event = location_name
+                    self.timer_handle = self.run_at(self.notify_user,datetime.datetime.now())
 
     def notify_user(self, *kwargs):
         if self.get_state(self.notify_input_boolean) == "on":
@@ -95,7 +95,8 @@ class NextApppointmentLeaveNotifier(hass.Hass):
             else:
                 google_maps_url = self.google_source_url + location_name.replace(" ","+")
                 self.log("Notify user")
-                self.notifier.notify(self.notify_name, self.message.format(location_name,self.get_state(self.travel_time_sensor), google_maps_url))
+                self.notifier.notify(self.notify_name, self.message.format(location_name,self.get_state(self.travel_time_sensor)))
+                self.notifier.notify(self.notify_name, self.message.format(google_maps_url), useAlexa=False)
                 self.location_of_last_notified_event = location_name
         else:
             self.log("Notification is turned off")
