c8b550fbbd24ec9180d19da9e201bb6ae76d95aa
Kevin Eifinger <k.eifinger@googlemail.com>
2018-11-30 10:11:51 +0100

    [ResetCounterOnLastDayOfMonth] Version 1.1: #   Increase day by one because different timezone

--- resetCounterOnLastDayOfMonth/resetCounterOnLastDayOfMonth.py
+++ resetCounterOnLastDayOfMonth/resetCounterOnLastDayOfMonth.py
@@ -7,50 +7,53 @@ import globals
 # Args:
 #
 # app_switch: on/off switch for this app. example: input_boolean.warm_bath_before_wakeup
-# time: domain of the service call. example: homeassistant
-# counter: increase a counter each time the service gets called. example: counter.google_maps_api_calls 
+# time: When to reset. example: "9:00"
+# counter: which counter to reset example: counter.google_maps_api_calls
 #
 # Release Notes
+#
+# Version 1.1:
+#   Increase day by one because different timezone
 #
 # Version 1.0:
 #   Initial Version
 
+
 class ResetCounterOnLastDayOfMonth(hass.Hass):
 
-  def initialize(self):
-    self.timer_handle_list = []
+    def initialize(self):
+        self.timer_handle_list = []
 
-    self.app_switch = globals.get_arg(self.args,"app_switch")
-    self.time = globals.get_arg(self.args,"time")
-    self.counter = globals.get_arg(self.args,"counter")
+        self.app_switch = globals.get_arg(self.args, "app_switch")
+        self.time = globals.get_arg(self.args, "time")
+        self.counter = globals.get_arg(self.args, "counter")
 
-    runtime = self.get_next_run_time()
-    self.log("Next reset will be: {}".format(runtime))
-    self.timer_handle_list.append(self.run_at(self.run_at_callback, runtime))
+        runtime = self.get_next_run_time()
+        self.log("Next reset will be: {}".format(runtime))
+        self.timer_handle_list.append(self.run_at(self.run_at_callback, runtime))
 
-  def run_at_callback(self, kwargs):
-    if self.get_state(self.app_switch) == "on":
-      self.call_service("counter/reset", entity_id=self.counter)
-      self.log("Reset {} to {}".format(self.friendly_name(self.counter), self.get_state(self.counter)))
-      runtime = self.get_next_run_time()
-      self.log("Next reset will be: {}".format(runtime))
-      self.timer_handle_list.append(self.run_at(self.run_at_callback, runtime))
+    def run_at_callback(self, kwargs):
+        if self.get_state(self.app_switch) == "on":
+            self.call_service("counter/reset", entity_id=self.counter)
+            self.log("Reset {} to {}".format(self.friendly_name(self.counter), self.get_state(self.counter)))
+            runtime = self.get_next_run_time()
+            self.log("Next reset will be: {}".format(runtime))
+            self.timer_handle_list.append(self.run_at(self.run_at_callback, runtime))
 
-  def last_day_of_month(self, any_day):
-    next_month = any_day.replace(day=28) + datetime.timedelta(days=4)  # this will never fail
-    return next_month - datetime.timedelta(days=next_month.day)
+    @staticmethod
+    def last_day_of_month(any_day):
+        next_month = any_day.replace(day=28) + datetime.timedelta(days=4)  # this will never fail
+        return next_month - datetime.timedelta(days=next_month.day)
 
-  def get_next_run_time(self):
-    today = datetime.date.today()
-    last_day_of_month = self.last_day_of_month(today)
-    hours = self.time.split(":",1)[0]
-    minutes = self.time.split(":",1)[1]
-    runtime = datetime.time(int(hours),int(minutes))
+    def get_next_run_time(self):
+        today = datetime.date.today()
+        last_day_of_month = self.last_day_of_month(today) + datetime.timedelta(days=1)  # Reset in in different timezone
+        hours = self.time.split(":", 1)[0]
+        minutes = self.time.split(":", 1)[1]
+        runtime = datetime.time(int(hours), int(minutes))
 
-    return datetime.datetime.combine(last_day_of_month, runtime)
-    
+        return datetime.datetime.combine(last_day_of_month, runtime)
 
-  def terminate(self):
-    for timer_handle in self.timer_handle_list:
-      self.cancel_timer(timer_handle)
-      
+    def terminate(self):
+        for timer_handle in self.timer_handle_list:
+            self.cancel_timer(timer_handle)
