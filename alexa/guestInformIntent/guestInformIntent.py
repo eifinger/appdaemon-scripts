1937fbf43a62408f3f3b29bb644ac5ce11d7be01
Kevin Eifinger <k.eifinger@googlemail.com>
2018-12-18 21:49:07 +0100

    [guestInformIntent] add guestInformIntent

---
+++ alexa/guestInformIntent/guestInformIntent.py
@@ -0,0 +1,25 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+class guestInformIntent(hass.Hass):
+
+    def initialize(self):
+        return
+
+    def getIntentResponse(self, slots, devicename):
+        ############################################
+        # an Intent to give back the state from a light.
+        # but it also can be any other kind of entity
+        ############################################           
+        try:
+            text = "Das weiß ich leider im Moment nicht"
+        except:
+            text = self.random_arg(self.args["Error"])
+        return text
+
+    def floatToStr(self, myfloat):
+        ############################################
+        # replace . with , for better speech
+        ############################################
+        floatstr = str(myfloat)
+        floatstr = floatstr.replace(".",",")
+        return floatstr
