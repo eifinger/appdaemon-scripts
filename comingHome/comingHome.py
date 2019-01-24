ac9f950893cd13a422081ecf5b754a561bc98340
Kevin Eifinger <k.eifinger@googlemail.com>
2019-01-24 09:25:24 +0100

    [ComingHome] Version 1.3.2: Check for new != old

--- comingHome/comingHome.py
+++ comingHome/comingHome.py
@@ -13,6 +13,9 @@ import datetime
 # after_sundown (optionally): whether to only trigger after sundown. example: True
 # Release Notes
 #
+# Version 1.3.2:
+#   Check for new != old
+#
 # Version 1.3.1:
 #   Actually implement isHome
 #
@@ -28,40 +31,43 @@ import datetime
 # Version 1.0:
 #   Initial Version
 
+
 class ComingHome(hass.Hass):
 
-  def initialize(self):
-    self.listen_state_handle_list = []
+    def initialize(self):
+        self.listen_state_handle_list = []
 
-    self.app_switch = globals.get_arg(self.args,"app_switch")
-    self.sensor = globals.get_arg(self.args,"sensor")
-    self.isHome = globals.get_arg(self.args,"isHome")
-    self.actor = globals.get_arg(self.args,"actor")
-    try:
-      self.after_sundown = globals.get_arg(self.args,"after_sundown")
-    except KeyError:
-      self.after_sundown = None
+        self.app_switch = globals.get_arg(self.args, "app_switch")
+        self.sensor = globals.get_arg(self.args, "sensor")
+        self.isHome = globals.get_arg(self.args, "isHome")
+        self.actor = globals.get_arg(self.args, "actor")
+        try:
+            self.after_sundown = globals.get_arg(self.args, "after_sundown")
+        except KeyError:
+            self.after_sundown = None
 
-    self.delay = 2
+        self.delay = 2
 
-    self.listen_state_handle_list.append(self.listen_state(self.state_change, self.sensor))
-    
-  def state_change(self, entity, attribute, old, new, kwargs):
-    if self.get_state(self.app_switch) == "on":
-      if new != "":
-        isHome_attributes = self.get_state(self.isHome, attribute = "all")
-        isHome_state = isHome_attributes["state"]
-        last_changed = self.convert_utc(isHome_attributes["last_changed"])
-        if isHome_state == "off" or (datetime.datetime.now(datetime.timezone.utc) - last_changed <= datetime.timedelta(seconds=self.delay)):
-          if self.after_sundown != None and self.after_sundown:
-            if self.sun_down():
-              self.log("{} changed to {}".format(self.friendly_name(entity), new))
-              self.turn_on(self.actor)
-          else:
-            self.log("{} changed to {}".format(self.friendly_name(entity), new))
-            self.turn_on(self.actor)
+        self.listen_state_handle_list.append(self.listen_state(self.state_change, self.sensor))
 
-  def terminate(self):
-    for listen_state_handle in self.listen_state_handle_list:
-      self.cancel_listen_state(listen_state_handle)
-      
+    def state_change(self, entity, attribute, old, new, kwargs):
+        if self.get_state(self.app_switch) == "on":
+            if new != "" and new != old:
+                isHome_attributes = self.get_state(self.isHome, attribute="all")
+                isHome_state = isHome_attributes["state"]
+                last_changed = self.convert_utc(isHome_attributes["last_changed"])
+                if (
+                        isHome_state == "off"
+                        or (datetime.datetime.now(datetime.timezone.utc) - last_changed <= datetime.timedelta(seconds=self.delay))
+                ):
+                    if self.after_sundown is not None and self.after_sundown:
+                        if self.sun_down():
+                            self.log("{} changed to {}".format(self.friendly_name(entity), new))
+                            self.turn_on(self.actor)
+                    else:
+                        self.log("{} changed to {}".format(self.friendly_name(entity), new))
+                        self.turn_on(self.actor)
+
+    def terminate(self):
+        for listen_state_handle in self.listen_state_handle_list:
+            self.cancel_listen_state(listen_state_handle)
