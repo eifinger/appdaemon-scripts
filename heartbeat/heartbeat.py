96b103e775426700eabd690238cb4653c008d38a
Kevin Eifinger <k.eifinger@googlemail.com>
2019-01-29 10:31:26 +0100

    add appdaemon heartbeat

---
+++ heartbeat/heartbeat.py
@@ -0,0 +1,30 @@
+import appdaemon.plugins.hass.hassapi as hass
+import globals
+#
+# App which sets a homeassistant entity as a heartbeat to check for threadstarvation etc
+#
+# Args:
+# sensor: sensor.appdaemon_heartbeat
+#
+# Release Notes
+#
+# Version 1.0:
+#   Initial Version
+
+
+class Heartbeat(hass.Hass):
+
+    def initialize(self):
+        self.timer_handle_list = []
+
+        self.sensor = globals.get_arg(self.args, "sensor")
+
+        self.timer_handle_list.append(self.run_minutely(self.heartbeat))
+
+    def heartbeat(self, kwargs):
+        self.set_state(self.sensor, state=self.time())
+        self.log("Heartbeat")
+
+    def terminate(self):
+        for timer_handle in self.timer_handle_list:
+            self.cancel_timer(timer_handle)
