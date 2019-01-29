3b20ca6cc4ea8e0139dad74c5678368f0b283f39
Kevin Eifinger <k.eifinger@googlemail.com>
2019-01-29 11:55:10 +0100

    fix for no appname at all

--- appWatcher/appWatcher.py
+++ appWatcher/appWatcher.py
@@ -30,14 +30,21 @@ class AppWatcher(hass.Hass):
     def log_message_callback(self, name, ts, level, message):
         if level == "WARNING" or level == "ERROR" or level == "CRITICAL":
             if name == "Appdaemon":
-                app_message_start_index = message.index(":", 11) + 2
-                app_name = message[11, message.index(":", 11)]
+                # check if this is a warning for an app
+                try:
+                    app_message_start_index = message.index(":", 11) + 2
+                except ValueError:
+                    app_message_start_index = None
+                first_space_index = message.index(" ", 11)
+                if app_message_start_index is not None:
+                    if app_message_start_index > first_space_index:
+                        app_name = message[11, message.index(":", 11)]
 
-                app_message = message[app_message_start_index:]
+                        app_message = message[app_message_start_index:]
 
-                self.notifier.notify(self.notify_name, self.notify_message.format(app_name), useAlexa=False)
-                if self.include_log_message_in_notification:
-                    self.notifier.notify(self.notify_name, app_message, useAlexa=False)
+                        self.notifier.notify(self.notify_name, self.notify_message.format(app_name), useAlexa=False)
+                        if self.include_log_message_in_notification:
+                            self.notifier.notify(self.notify_name, app_message, useAlexa=False)
 
     def terminate(self):
         self.cancel_listen_log()
