cede2587368a28898727737668f2c80867adeeb9
Kevin Eifinger <k.eifinger@googlemail.com>
2019-04-25 16:09:41 +0200

    [UserSleepModeHandler] Version 1.3: Reimplementation

--- sleepModeHandler/userSleepModeHandler.py
+++ sleepModeHandler/userSleepModeHandler.py
@@ -13,6 +13,9 @@ import globals
 #
 # Release Notes
 #
+# Version 1.3:
+#   Reimplementation
+#
 # Version 1.2:
 #   Only trigger on an actual change
 #
@@ -35,40 +38,51 @@ class UserSleepModeHandler(hass.Hass):
         self.awake_duration = globals.get_arg(self.args, "awake_duration")
         self.input_boolean = globals.get_arg(self.args, "input_boolean")
 
-        self.listen_state_handle_list.append(
-            self.listen_state(self.asleep, self.location_sensor, new=self.room, duration=self.asleep_duration))
-        self.listen_state_handle_list.append(
-            self.listen_state(self.awake, self.location_sensor, old=self.room, duration=self.awake_duration))
-    
-    def asleep(self, entity, attribute, old, new, kwargs):
-        if self.get_state(self.app_switch) == "on":
-            if new != "" and new != old:
-                if self.get_state(self.input_boolean) == "off":
-                    self.log(
-                        "{} is in {} for more than {}s. Turning {} on".format(
-                            self.friendly_name(self.location_sensor),
-                            self.room,
-                            self.asleep_duration,
-                            self.input_boolean
-                        )
-                    )
-                    self.turn_on(self.input_boolean)
+        self.timer_handle = None
 
-    def awake(self, entity, attribute, old, new, kwargs):
-        if self.get_state(self.app_switch) == "on":
-            if new != "" and new != old:
-                if self.get_state(self.input_boolean) == "on":
-                    self.log(
-                        "{} is outside {} for more than {}s. Turning {} off".format(
-                            self.friendly_name(self.location_sensor),
-                            self.room,
-                            self.awake_duration,
-                            self.input_boolean
-                        )
-                    )
-                    self.turn_off(self.input_boolean)
+        self.listen_state_handle_list.append(
+            self.listen_state(self.state_change, self.location_sensor))
+    
+    def state_change(self, entity, attribute, old, new, kwargs):
+        """Handle state changes of the location sensor"""
+        if new != old:
+            if self.get_state(self.app_switch) == "on":
+                # User left room
+                if old == self.room:
+                    self.log(f"User left {self.room}. Resetting timer. Will trigger awake in {self.awake_duration}s")
+                    if self.timer_handle is not None:
+                        self.cancel_timer(self.timer_handle)
+                    self.timer_handle = self.run_in(self.awake, self.awake_duration)
+                elif new == self.room:
+                    self.log(f"User entered {self.room}. "
+                             f"Resetting timer. Will trigger asleep in {self.asleep_duration}s")
+                    if self.timer_handle is not None:
+                        self.cancel_timer(self.timer_handle)
+                    self.timer_handle = self.run_in(self.awake, self.asleep_duration)
+
+    def awake(self, kwargs):
+        """User left room for more than self.awake_duration. Turn off sleep mode"""
+        if self.get_state(self.input_boolean) == "on":
+            self.log(
+                f"{self.friendly_name(self.location_sensor)} is outside {self.room} "
+                f"for more than {self.asleep_duration}s. "
+                f"Turning {self.input_boolean} off")
+            self.turn_off(self.input_boolean)
+
+    def asleep(self, kwargs):
+        """User stayed in room for more than self.asleep_duration. Turn on sleep mode"""
+        if self.get_state(self.input_boolean) == "off":
+            self.log(
+                f"{self.friendly_name(self.location_sensor)} is in {self.room} "
+                f"for more than {self.asleep_duration}s. "
+                f"Turning {self.input_boolean} on")
+            self.turn_on(self.input_boolean)
+
 
     def terminate(self):
+        if self.timer_handle is not None:
+            self.cancel_timer(self.timer_handle)
+
         for listen_state_handle in self.listen_state_handle_list:
             self.cancel_listen_state(listen_state_handle)
 
