c43ba41f132d8c00dd2670a613f8fca90f9977a2
Kevin Eifinger <k.eifinger@googlemail.com>
2019-09-22 16:07:29 +0200

    refactor travelTimeNotifier

--- travelTimeNotifier/travelTimeNotifier.py
+++ travelTimeNotifier/travelTimeNotifier.py
@@ -63,7 +63,7 @@ class TravelTimeNotifier(hass.Hass):
             self.listen_state(self.state_change, self.sensor, attribute="all")
         )
 
-    def state_change(self, entity, attributes, old, new, kwargs):
+    def state_change(self, entity, attributes, old, new, kwargs) -> None:
         self.log("entity: {}".format(entity), level="DEBUG")
         self.log("old: {}".format(old), level="DEBUG")
         self.log("new: {}".format(new), level="DEBUG")
@@ -77,47 +77,64 @@ class TravelTimeNotifier(hass.Hass):
         duration_minutes = self.parse_duration_minutes(new)
         self.log("duration_minutes: {}".format(duration_minutes), level="DEBUG")
 
-        if (
-            duration_in_traffic_minutes <= duration_minutes * self.acceptable_range
-            and self.get_state(self.notify_input_boolean) == "on"
-        ):
-            destination_address = self.parse_destination_address(new)
-            message = self.message.format(destination_address)
-            self.log("Notify user")
-            self.notifier.notify(
-                self.notify_name, message, useAlexa=self.notify_use_Alexa
-            )
-            self.turn_off(self.notify_input_boolean)
+        if duration_in_traffic_minutes <= duration_minutes * self.acceptable_range:
+            if self.get_state(self.notify_input_boolean) == "on":
+                destination_address = self.parse_destination_address(new)
+                self.notify_user(destination_address)
+                self.turn_off(self.notify_input_boolean)
 
-    @staticmethod
-    def parse_duration_in_traffic_minutes(state):
-        duration_in_traffic = state["attributes"]["duration_in_traffic"]
-        if isinstance(duration_in_traffic, float):
-            duration_in_traffic_minutes = int(duration_in_traffic)
+    def notify_user(self, address: str) -> None:
+        """Notify the user it is time to leave for the given address."""
+        message = self.message.format(address)
+        self.log("Notify user")
+        self.notifier.notify(self.notify_name, message, useAlexa=self.notify_use_Alexa)
+
+    def parse_duration_in_traffic_minutes(self, state) -> Optional[int]:
+        """Get duration_in_traffic from the states attributes."""
+        duration_in_traffic = state["attributes"].get("duration_in_traffic")
+        duration_in_traffic_minutes = None
+        if duration_in_traffic is not None:
+            if isinstance(duration_in_traffic, float):
+                duration_in_traffic_minutes = int(duration_in_traffic)
+            else:
+                duration_in_traffic_minutes = int(
+                    duration_in_traffic[: duration_in_traffic.find(" ")]
+                )
         else:
-            duration_in_traffic_minutes = int(
-                duration_in_traffic[: duration_in_traffic.find(" ")]
+            self.log(
+                "Could not find duration_in_traffic in state attributes.",
+                level="WARNING",
             )
         return duration_in_traffic_minutes
 
-    @staticmethod
-    def parse_destination_address(state):
+    def parse_destination_address(self, state) -> Optional[str]:
+        """Get a destination address from the states attributes."""
         attributes = state["attributes"]
+        destination_address = None
         if "destination_name" in attributes:
             destination_address = attributes["destination_name"]
         elif "destination_addresses" in attributes:
             destination_address = attributes["destination_addresses"][0]
+        else:
+            self.log(
+                "Could not find destination_name or destination_addresses in state attributes.",
+                level="WARNING",
+            )
         return destination_address
 
-    @staticmethod
-    def parse_duration_minutes(state):
-        duration = state["attributes"]["duration"]
-        if isinstance(duration, float):
-            duration_minutes = int(duration)
+    def parse_duration_minutes(self, state) -> Optional[int]:
+        """Get duration from the states attributes."""
+        duration_minutes = None
+        duration = state["attributes"].get("duration")
+        if duration is not None:
+            if isinstance(duration, float):
+                duration_minutes = int(duration)
+            else:
+                duration_minutes = int(duration[: duration.find(" ")])
         else:
-            duration_minutes = int(duration[: duration.find(" ")])
+            self.log("Could not find duration in state attributes.", level="WARNING")
         return duration_minutes
 
-    def terminate(self):
+    def terminate(self) -> None:
         for listen_state_handle in self.listen_state_handle_list:
             self.cancel_listen_state(listen_state_handle)
